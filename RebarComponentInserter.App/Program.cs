f95bc9c40a871f1f8c79938d611dc68404387268
Pavel Karpovich <karpovich.pavel@gmail.com>
2026-05-20 09:19:41 +0300

    feat: experiments with target frameworks

---
+++ RebarComponentInserter.App/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RebarComponentInserter.App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RebarComponentInserter.TestClass.Create();
+        }
+    }
+}
---
+++ RebarComponentInserter/TestClass.cs
@@ -0,0 +1,29 @@
+﻿using Tekla.Structures.Geometry3d;
+using Tekla.Structures.Model;
+
+namespace RebarComponentInserter
+{
+    public static class TestClass
+    {
+        public static void Create()
+        {
+            Beam beam = new Beam()
+            {
+                StartPoint = new Point(),
+                EndPoint = new Point(10000, 0, 0),
+                AssemblyNumber = new NumberingSeries(),
+                Class = "2",
+                CastUnitType = Part.CastUnitTypeEnum.CAST_IN_PLACE,
+                EndPointOffset = new Offset(),
+                StartPointOffset = new Offset(),
+                Material = new Material() { MaterialString = "Steel_Undefined" },
+                Name = "BEAM",
+                Profile = new Profile() { ProfileString = "200*500" },
+                PartNumber = new NumberingSeries(),
+                Position = new Position(),
+            };
+
+            beam.Insert();
+        }
+    }
+}
